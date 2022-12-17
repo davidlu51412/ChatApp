bb11fdc8251b03d785c3a6f95dbbcdd67f739a08
David Lu <davidlu51412@gmail.com>
2022-12-17 16:21:50 -0500

    increase queue time

--- frontend/src/globals/globals.js
+++ frontend/src/globals/globals.js
@@ -1,7 +1,7 @@
 export const socketURL = process.env.REACT_APP_SOCKET_URL;
 export const restURL = process.env.REACT_APP_REST_URL;
 
-export const maxMsInQueue = 5 * 1000; // in ms
+export const maxMsInQueue = 1 * 60 * 1000; // in ms
 export const maxChatTime = 5 * 60 * 1000; // in ms
 
 export let ws = null;
