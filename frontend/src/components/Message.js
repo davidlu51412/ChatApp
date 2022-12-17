e729bf96a884d4fae930e8bbf19009a178cdba52
David Lu <davidlu51412@gmail.com>
2022-12-17 14:54:56 -0500

    github pages

--- frontend/src/components/Message.js
+++ frontend/src/components/Message.js
@@ -2,8 +2,8 @@ import React from "react";
 
 function Message({ text, isUser }) {
   const styling = !isUser
-    ? "bg-zinc-700 text-zinc-200 rounded-lg float-left p-2 max-w-[60%]"
-    : "bg-zinc-200 text-zinc-700 rounded-lg float-right p-2 max-w-[60%]";
+    ? "bg-zinc-700 text-zinc-200 rounded-lg float-left p-2 max-w-[60%] break-all"
+    : "bg-zinc-200 text-zinc-700 rounded-lg float-right p-2 max-w-[60%] break-all";
   return (
     <div class="w-full overflow-auto">
       <p class={styling}>{text}</p>
